45a8479ce29e010755436f6dbf9a807f30cb9072
txl302 <txl302@case.edu>
2020-06-02 16:21:21 -0400

    expr_test

--- Rin_embedded/Rin_faces.py
+++ Rin_embedded/Rin_faces.py
@@ -5,12 +5,13 @@ import random
 from collections import namedtuple
 
 
-SHOW = '*'
+SHOW = '\033[36m#'
 EMPTY = ' '
 
 
 
 class Face(object):
+
     def __init__(self):
         self.height = 32
         self.width = 80
@@ -26,13 +27,89 @@ class Face(object):
             else:
                 self.rows.append([EMPTY]*12 + [SHOW]*16 + [EMPTY]*24 + [SHOW]*16 + [EMPTY]*12)
 
-    def twinkle(self):
+    def close(self):
         self.rows = []
         for i in range(self.height):
             if (i<15 or i>16):
                 self.rows.append([EMPTY] * self.width)
             else:
-                self.rows.append([EMPTY]*12 + [SHOW]*16 + [EMPTY]*24 + [SHOW]*16 + [EMPTY]*12)
+                self.rows.append([EMPTY]*10 + [SHOW]*20 + [EMPTY]*20 + [SHOW]*20 + [EMPTY]*10)
+
+    def twinkle(self):
+
+        clear = '\x1b[{}A\x1b[{}D'.format(80,32)
+        self.close()
+
+        print(clear)
+        print(self)
+        time.sleep(.1)
+
+        self.neutral()
+
+        print(clear)
+        print(self)
+        time.sleep(.1)
+
+        self.close()
+
+        print(clear)
+        print(self)
+        time.sleep(.1)
+
+        self.neutral()
+
+        print(clear)
+        print(self)
+        time.sleep(2)
+
+    def look_left(self):
+        self.rows = []
+        for i in range(self.height):
+            if (i<3 or i>22):
+                self.rows.append([EMPTY] * self.width)
+            else:
+                self.rows.append([EMPTY]*6 + [SHOW]*16 + [EMPTY]*24 + [SHOW]*16 + [EMPTY]*18)
+
+    def look_right(self):
+        self.rows = []
+        for i in range(self.height):
+            if (i<3 or i>22):
+                self.rows.append([EMPTY] * self.width)
+            else:
+                self.rows.append([EMPTY]*18 + [SHOW]*16 + [EMPTY]*24 + [SHOW]*16 + [EMPTY]*6)
+
+    def look_around(self):
+        clear = '\x1b[{}A\x1b[{}D'.format(80,32)
+        self.look_left()
+
+        print(clear)
+        print(self)
+        time.sleep(1)
+
+        self.look_right()
+
+        print(clear)
+        print(self)
+        time.sleep(1)
+
+        self.neutral()
+
+        print(clear)
+        print(self)
+        time.sleep(1)
+
+    def normal(self):
+        clear = '\x1b[{}A\x1b[{}D'.format(80,32)
+
+        self.neutral()
+
+        print(clear)
+        print(self)
+        time.sleep(1)
+
+
+
+
 
 
 
@@ -46,24 +123,19 @@ class Face(object):
 
 
 
-
-
 def test():
+    os.system('clear')
     g = Face()
     clear = '\x1b[{}A\x1b[{}D'.format(80,32)
     while True:
-
-        g.neutral()
-
-        print(clear)
-        print(g)
-        time.sleep(3)
-
+        g.normal()
+        g.look_around()
+        g.twinkle()
         g.twinkle()
 
-        print(clear)
-        print(g)
-        time.sleep(.1)
+
+
+        
 
 
     
